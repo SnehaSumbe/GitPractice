20540ad5460a7ae008782a466c58fd9445e5e540
Sneha <sumbesneha@gmail.com>
2022-10-04 07:05:47 +0530

    Java Changed

---
+++ initial.java
@@ -0,0 +1,10 @@
+Class Initial{
+       public static void main(String[] args) {
+        
+        String name= new String("SNEHA");
+        System.out.println(name);
+        
+        
+    }
+        
+    }
